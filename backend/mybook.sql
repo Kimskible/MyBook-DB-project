a854699d2ca3ec01b05ddf91d7818687f3a5f630
anthonywilliamsjj <anthonywilliamsjj@users.noreply.github.com>
2020-05-12 18:19:38 -0500

    Added Procedures

--- backend/mybook.sql
+++ backend/mybook.sql
@@ -98,4 +98,102 @@ CREATE TABLE IF NOT EXISTS `Creators`(
 /*ALTER TABLE `User` ADD `datejoined` DATE NOT NULL;*/
 
 
+-- ACCEPT FRIEND REQUEST - Accept ID and Update Friends Table
+DELIMITER //;
+	CREATE Procedure AddFriend(IN newFriendID int(10),newUserID int(10)) 
+	BEGIN Insert into Friends values(newFriendID,newUserID,null)
+	END //;
+DELIMITER ;
+
+-- REMOVE FRIEND
+DELIMITER //;
+	CREATE Procedure RemoveFriend(IN friendID int(10)) 
+	BEGIN 
+	Delete from Friends where fnd_ID = friendID;
+	END //;
+DELIMITER ;
+
+
+
+--ADD POST
+DELIMITER //;
+	CREATE Procedure NewPost(IN postID int(10),userID int(10),textPost varchar(50),imgPost text) 
+	BEGIN 
+	Insert into Post values(postID,userID,textPost,imgPost,CONVERT (date, GETDATE()),CONVERT (time, CURRENT_TIMESTAMP));
+	END //;
+DELIMITER ;
+
+--REMOVE POST
+	DELIMITER //;
+	CREATE Procedure DeletePost(IN postID, int(10)) 
+	BEGIN Delete from Post where post_id = postID;
+	END //;
+DELIMITER ;
+
+--ADD COMMENT this procedure assumes that the comment table would have a comment ID attribute but it does not currently have it
+DELIMITER //;
+	CREATE Procedure AddComment(IN commentID, int(10)) 
+	BEGIN 
+	Insert into Comments values(newFriendID,newUserID,null);
+	END //;
+DELIMITER ;
+
+--DELETE COMMENT this procedure assumes that the comment table would have a comment ID attribute but it does not currently have it
+DELIMITER //;
+	CREATE Procedure DeleteComment(IN commentID, int(10)) 
+	BEGIN 
+	Delete from Comments where cmt_ID = commentID;
+	END //;
+DELIMITER ;
+  
+  
+--CREATE NEW GROUP
+DELIMITER //;
+	CREATE Procedure AddGrp(IN groupID (10), memberID (10)) 
+	BEGIN 
+	Insert into Groups values(groupID,memberID,null);
+	END //;
+DELIMITER ;
+
+
+ --DELETE GROUP
+DELIMITER //;
+	CREATE Procedure DeleteGrp(IN groupID (10)) 
+	BEGIN 
+	Delete from Groups where grp_id=groupID;
+	END //;
+DELIMITER ;
+  
+--ADD USER TO GROUP
+DELIMITER //;
+	CREATE Procedure AddGrpMember(IN groupID int(10),memberID int(10)) 
+	BEGIN 
+	Insert into Groups values(groupID,memberID,null);
+	END //;
+DELIMITER ;
+
+--REMOVE USER FROM GROUP
+DELIMITER //;
+	CREATE Procedure RemoveGrpMember(IN memberID int(10)) 
+	BEGIN 
+	Delete from Groups where user_id = commentID;
+	END //;
+DELIMITER ; 
+ 
+--ADD POST TO GROUP
+
+DELIMITER //;
+	CREATE Procedure AddGrpPost(IN groupID(10), userID(10), postID (10))
+	BEGIN 
+	Insert into Groups values(groupID,userID,postID);
+	END //;
+DELIMITER ; 
+
+--REMOVE POST FROM GROUP
+DELIMITER //;
+	CREATE Procedure RemoveGrpPost(IN groupID(10), postID (10)) 
+	BEGIN 
+	Delete from Groups where (grp_id=groupID) and (post_id = postID);
+	END //;
+DELIMITER ; 
 
