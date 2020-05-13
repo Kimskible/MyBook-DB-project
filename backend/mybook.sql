6f2a406951111680c80589ba92b1ba2ebc5c5ee4
Kimski <k_oksana15@hotmail.com>
2020-05-12 23:28:53 -0500

    updated databases

--- backend/mybook.sql
+++ backend/mybook.sql
@@ -1,6 +1,8 @@
+use mybook; 
+
 CREATE TABLE IF NOT EXISTS `User`(
-  `user_id` int(10) NOT NULL AUTO_INCREMENT,
-  `fname` varchar(30) DEFAULT NULL,
+ `user_id` int(10) NOT NULL AUTO_INCREMENT,
+ `fname` varchar(30) DEFAULT NULL,
   `lname` varchar(30) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `pwd` varchar(30) DEFAULT NULL,
@@ -17,146 +19,142 @@ CREATE TABLE IF NOT EXISTS `Profile` (
    FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE
  );
 
+CREATE TABLE IF NOT EXISTS `Post`(
+  `post_id` int(10) NOT NULL auto_increment,
+  `user_id` int(10) NOT NULL,
+  `text_post` varchar(50) DEFAULT NULL,
+  `img_post` text,
+  `post_date` DATE,
+  `post_time` TIME,
+  PRIMARY KEY (`post_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `Groups`(
+  `grp_name` varchar(25) NOT NULL,
+  `members_id` int not null,
+  FOREIGN KEY (`members_id`) REFERENCES `GroupMembers`(`members_id`) ON DELETE CASCADE
+ );
+ 
+ CREATE TABLE IF NOT EXISTS `GroupMembers`(
+  `groupmember_id` int not null auto_increment,
+  `members_id` int not null,
+  `user_id` int not null,
+  PRIMARY KEY (`groupmember_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE CASCADE
+ );
+
+CREATE TABLE IF NOT EXISTS `Friends`(
+  `user_id` int(10) NOT NULL,
+  `user_id2` int NOT NULL,
+  `fnd_id` int(10) NOT NULL auto_increment,
+  PRIMARY KEY (`fnd_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `Relatives`(
+  `rel_id` int(10) NOT NULL auto_increment,
+  `user_id` int(10) NOT NULL,
+   `user_id2` int NOT NULL,
+  PRIMARY KEY (`rel_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `Work`(
+  `work_id` int(10) NOT NULL auto_increment,
+  `user_id` int(10) NOT NULL,
+  `user_id2` int NOT NULL,
+  PRIMARY KEY (`work_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `School`(
+  `sch_id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `user_id2` int NOT NULL,
+  PRIMARY KEY (`sch_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `Comments`(
+  `comt_id` int(10) NOT NULL auto_increment,
+  `user_id` int(10) NOT NULL,
+  `cmt_date` DATE,
+  `cmt_time` TIME,
+  PRIMARY KEY(`comt_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `Creators`(
+  `crt_id` int(10) NOT NULL auto_increment,
+  `user_id` int(10) NOT NULL,
+  `grp_id` int(10) NOT NULL,
+  PRIMARY KEY (`crt_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`grp_id`) REFERENCES `Group` (`grp_id`) ON DELETE CASCADE
+);
 
 
--- CREATE TABLE IF NOT EXISTS `Post`(
---   `post_id` int(10) NOT NULL,
---   `user_id` int(10) NOT NULL,
---   `text_post` varchar(50) DEFAULT NULL,
---   `img_post` text,
---   `post_date` DATE,
---   `post_time` TIME,
---   PRIMARY KEY (`post_id`),
---   FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE
+/*Stored Procedures*/
 
--- );
-
--- CREATE TABLE IF NOT EXISTS `Groups`(
---   `grp_id` int(10) NOT NULL, 
---   `user_id` int(10) NOT NULL,
---   `post_id` int(10) NOT NULL,
---   `rel_id` int(10) NOT NULL,
---   `sch_id` int(10) NOT NULL,
---   `work_id` int(10) NOT NULL,
---   `grptype` VARCHAR(20) DEFAULT NULL,
---   PRIMARY KEY (`grp_id`),
---   FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE CASCADE,
---   FOREIGN KEY (`post_id`) REFERENCES `Post`(`post_id`) ON DELETE CASCADE,
---   FOREIGN KEY (`rel_id`) REFERENCES `Relatives`(`rel_id`) ON DELETE CASCADE,
---   FOREIGN KEY (`work_id`) REFERENCES `Work`(`work_id`) ON DELETE CASCADE,
---   FOREIGN KEY (`sch_id`) REFERENCES `School`(`sch_id`) ON DELETE CASCADE
---  );
-
--- CREATE TABLE IF NOT EXISTS `Friends`(
---   `user_id` int(10) NOT NULL,
---   `fnd_id` int(10) NOT NULL,
---   `class` varchar(30) DEFAULT NULL,
---   PRIMARY KEY (`fnd_id`),
---   FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE CASCADE
--- );
-
--- CREATE TABLE IF NOT EXISTS `Relatives`(
---   `rel_id` int(10) NOT NULL,
---   `user_id` int(10) NOT NULL,
---   `grp_id` int(10) NOT NULL, 
---   PRIMARY KEY (`rel_id`),
---   FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE CASCADE,
---   FOREIGN KEY (`grp_id`) REFERENCES `Groups`(`grp_id`) ON DELETE CASCADE
--- );
-
--- CREATE TABLE IF NOT EXISTS `Work`(
---   `work_id` int(10) NOT NULL,
---   `user_id` int(10) NOT NULL,
---   `grp_id` int(10) NOT NULL, 
---   PRIMARY KEY (`work_id`),
---   FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE CASCADE,
---   FOREIGN KEY (`grp_id`) REFERENCES `Groups`(`grp_id`) ON DELETE CASCADE
--- );
-
--- CREATE TABLE IF NOT EXISTS `School`(
---   `sch_id` int(10) NOT NULL,
---   `user_id` int(10) NOT NULL,
---   `grp_id` int(10) NOT NULL, 
---   PRIMARY KEY (`sch_id`),
---   FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE CASCADE,
---   FOREIGN KEY (`grp_id`) REFERENCES `Groups`(`grp_id`) ON DELETE CASCADE
--- );
-
--- CREATE TABLE IF NOT EXISTS `Comments`(
---   `user_id` int(10) NOT NULL,
---   `cmt_date` DATE,
---   `cmt_time` TIME,
---  FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE
--- );
-
--- CREATE TABLE IF NOT EXISTS `Creators`(
---   `user_id` int(10) NOT NULL,
---   `grp_id` int(10) NOT NULL,
---   FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE,
---   FOREIGN KEY (`grp_id`) REFERENCES `Group` (`grp_id`) ON DELETE CASCADE
--- );
-
-
-/*ALTER TABLE `User` ADD `datejoined` DATE NOT NULL;
-
-ALTER TABLE `groups` RENAME TO `group_`;
-DROP TABLE `group`; */
-
-/*Procedures*/
+DELIMITER //
 
 /* Updating Profile*/
+CREATE PROCEDURE `UpdateProfile`(
+    IN id int,
+    IN firstN VARCHAR(30),
+    IN lastN VARCHAR(30),
+    IN dob_ DATE,
+    IN biography TEXT,
+    IN gen VARCHAR(2),
+    IN pro_pic TEXT
+)
+  BEGIN
+    UPDATE `Profile`
+    SET
+      dob = dob_,
+      bio = biography,
+      gender = gen,
+      profile_pic = pro_pic
+      WHERE user_id = id;
+      UPDATE `user`
+      SET 
+      fname = FirstN,
+      lname = LastN
+      WHERE user_id = id;
+  END //
+  
 
--- DELIMITER //
+/*ACCEPT FRIEND REQUEST - Accept ID and Update Friends Table*/
+	CREATE Procedure AddFriend(IN newFriendID int,newUserID int)
+    BEGIN
+		Insert into Friends values(newFriendID,newUserID);
+	END //
 
--- /* Updating Profile*/
--- CREATE PROCEDURE `UpdateProfile`(
---     IN id int,
---     IN firstN VARCHAR(30),
---     IN lastN VARCHAR(30),
---     IN dob_ DATE,
---     IN biography TEXT,
---     IN gen VARCHAR(2),
---     IN pro_pic TEXT,
--- )
---   BEGIN
---     UPDATE 'PROFILE'
---     SET
---       IN fname = firstN,
---       IN lastN = lname,
---       IN dob = dob,
---       IN biography = bio,
---       IN gen = gender,
---       IN pro_pic = profile_pic,
---       WHERE user_id = id
+ /*REMOVE FRIEND*/
+CREATE Procedure RemoveFriend(IN friendID int)
+	BEGIN 
+		Delete from Friends where fnd_id = friendID;
+	END //
 
---   END//
+/*ADD POST*/
+CREATE Procedure NewPost(userID int,textPost varchar(50), imgPost text) 
+	BEGIN 
+		Insert into Post(user_id, text_post, img_post, post_date, post_time) values (userID,textPost,imgPost, CURDATE(), CURTIME());
+	END //
+    
+/*REMOVE POST*/
+CREATE Procedure DeletePost(IN postID int) 
+	BEGIN
+		Delete from Post where post_id = postID;
+	END //
 
-/*Creating a group*/
-
-
-/*Making a post*/
-
-
-/*Adding a Friend*/
+/*View Friends list*/
+Create Procedure FriendList(IN userID int)
+		BEGIN
+			select friends.user_id2, user.fname, user.lname from `friends` JOIN `user` 
+      where friends.user_id = userID and friends.user_id2 = user.user_id;
+		END //
 
 
-/*Remove a Friend*/
-
-
-/*Send Request */
-
-
-/*Decline Request*/
-
-
-/* Accepting a Friend */
-
-
-/*Making a Post*/
-
-
-/* */
-
-
-
--- DELIMITER;
+DELIMITER ;
